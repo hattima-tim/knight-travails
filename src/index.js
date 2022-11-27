6bab23a1d52e5b4a9814e2a1d84600af3da7f8de
hattima-tim <irfanridwan20@gmail.com>
2022-11-27 12:40:00 +0000

    create functionality for getting end point

--- src/index.js
+++ src/index.js
@@ -18,6 +18,47 @@ const createKnight = (i, yAxis) => {
   return knightIcon;
 };
 
+const removePrevEndPoint = () => {
+  const prevEndPoint = document.querySelector('.endPoint');
+  if (prevEndPoint) {
+    prevEndPoint.classList.remove('endPoint');
+    prevEndPoint.style.background = 'none';
+  }
+};
+
+let endPoint = null;
+const createGameBoardSquares = (i) => {
+  const squareDiv = [];
+  let loopCount = 1;
+  let yAxis = 0;
+
+  squareDiv[i] = document.createElement('div');
+
+  if (i === 0) {
+    const knightIcon = createKnight(i, yAxis);
+    squareDiv[i].appendChild(knightIcon);
+    createDragDropFunctionality(knightIcon);
+  }
+
+  squareDiv[i].setAttribute('data-index', `${[i, yAxis]}`);
+  if (loopCount === 8) {
+    yAxis += 1;
+    loopCount = 1;
+  } else {
+    loopCount += 1;
+  }
+  squareDiv[i].classList.add('square_div');
+
+  squareDiv[i].addEventListener('click', (e) => {
+    removePrevEndPoint();
+
+    e.target.classList.add('endPoint');
+    e.target.style.background = 'red';
+    endPoint = e.target.dataset.index;
+  });
+  gameboard.appendChild(squareDiv[i]);
+};
+
 const createGameBoardDom = () => {
   const gridSize = 8;
   const gridSquare = 64;
@@ -26,27 +67,8 @@ const createGameBoardDom = () => {
   gameboard.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
   gameboard.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
 
-  const squareDiv = [];
-  let loopCount = 1;
-  let yAxis = 0;
   for (let i = 0; i < gridSquare; i += 1) {
-    squareDiv[i] = document.createElement('div');
-
-    if (i === 0) {
-      const knightIcon = createKnight(i, yAxis);
-      squareDiv[i].appendChild(knightIcon);
-      createDragDropFunctionality(knightIcon);
-    }
-
-    squareDiv[i].setAttribute('data-index', `${[i, yAxis]}`);
-    if (loopCount === 8) {
-      yAxis += 1;
-      loopCount = 1;
-    } else {
-      loopCount += 1;
-    }
-    squareDiv[i].classList.add('square_div');
-    gameboard.appendChild(squareDiv[i]);
+    createGameBoardSquares(i);
   }
 };
 createGameBoardDom();
