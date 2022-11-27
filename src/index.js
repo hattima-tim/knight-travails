ae2a38b94b8bc0b8dce56f9327130cdb1fef4d62
hattima-tim <irfanridwan20@gmail.com>
2022-11-27 10:48:58 +0000

    create knight

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import knightMoves from './knightTravails';
 
 const gameboard = document.querySelector('#gameboard');
 
+const createKnight = (i, yAxis) => {
+  const knightIcon = document.createElement('img');
+
+  knightIcon.classList.add('knight');
+  knightIcon.classList.add('draggable');
+
+  knightIcon.setAttribute(
+    'src',
+    'https://res.cloudinary.com/du3oueesv/image/upload/v1669444959/knights-travails/chess_fc13db.png',
+  );
+  knightIcon.setAttribute('data-index', `${[i, yAxis]}`);
+
+  return knightIcon;
+};
+
 const createGameBoardDom = () => {
   const gridSize = 8;
   const gridSquare = 64;
@@ -15,6 +30,12 @@ const createGameBoardDom = () => {
   let yAxis = 0;
   for (let i = 0; i < gridSquare; i += 1) {
     squareDiv[i] = document.createElement('div');
+
+    if (i === 0) {
+      const knightIcon = createKnight(i, yAxis);
+      squareDiv[i].appendChild(knightIcon);
+    }
+
     squareDiv[i].setAttribute('data-index', `${[i, yAxis]}`);
     if (loopCount === 8) {
       yAxis += 1;
